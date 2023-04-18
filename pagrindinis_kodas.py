32510ca00b9cb5ed6de419845973d345cc0aa959
Honichiwa <tadas.zupka@gmail.com>
2023-04-18 13:09:00 +0300

    kazka

--- pagrindinis_kodas.py
+++ pagrindinis_kodas.py
@@ -2,28 +2,46 @@ import os
 
 os.system("cls")
 
-produktai = {'Skystas': {}, 'Kietas': {}, 'Paruoštas': {}}
+produktai = {"Skystas": {}, "Kietas": {}, "Paruoštas": {}}
+
 
 def produkto_tipas():
-    print('-------[ Produkto tipas ]-------')
-    print('1: Skystas produktas')
-    print('2: Kietas produktas')
-    print('3: Paruoštas')
-    pasirinkimas = int(input('Įveskite pasirinkimą:'))
+    print("-------[ Produkto tipas ]-------")
+    print("1: Skystas produktas")
+    print("2: Kietas produktas")
+    print("3: Paruoštas")
+    pasirinkimas = int(input("Įveskite pasirinkimą:"))
 
     if pasirinkimas == 1:
-        produktas = input('Įveskite produktą: ')
-        kiekis = int(input('Įveskite produkto kiekį litrais: '))
-        produktai['Skystas'][produktas]= kiekis
+        produktas = input("Įveskite produktą: ")
+        kiekis = int(input("Įveskite produkto kiekį litrais: "))
+        produktai["Skystas"][produktas] = kiekis
         print(produktai)
     elif pasirinkimas == 2:
-        produktas = input('Įveskite produktą: ')
-        kiekis = input('Įveskite produkto kiekį kilogramais: ')
-        produktai['Kietas'][produktas]= kiekis
+        produktas = input("Įveskite produktą: ")
+        kiekis = input("Įveskite produkto kiekį kilogramais: ")
+        produktai["Kietas"][produktas] = kiekis
     else:
-        produktas = int(input('Įveskite produktą: '))
-        kiekis = int(input('Įveskite produkto kiekį vienetais: '))
-        produktai['Kietas'][produktas]= kiekis
+        produktas = str(input("Įveskite produktą: "))
+        kiekis = int(input("Įveskite produkto kiekį vienetais: "))
+        produktai["Kietas"][produktas] = kiekis
+
+
+def perziureti_saldytuva():
+    os.system("cls")
+
+    if list(produktai.keys()) != []:
+        for indeksas, reiksme in enumerate(produktai):
+            indeksas += 1
+            print(indeksas, ".", reiksme, produktai[reiksme])
+    else:
+        print("nera sukurtu uzduociu")
+
+
+def saldytuvo_sudetis():
+    visi_produktai = list(produktai["Skystas"].values)
+    return visi_produktai
+
 
 while True:
     os.system("cls")
@@ -61,6 +79,7 @@ while True:
 
     if pasirinkimas == "2":  # antras pasirinkimas
         os.system("cls")
+        perziureti_saldytuva()
         while True:
             pasirinkimas_3 = input("ivevskite 0 gryzti i menu \n\u2794 ")
             if pasirinkimas_3 == "0":
